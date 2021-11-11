df4819e4e29d76ec9c0e83102b587cfeb41f29c5
Drew Pomatti <75772886+thedrewery@users.noreply.github.com>
2021-11-11 12:43:20 -0800

    Add files via upload

---
+++ event.js
@@ -0,0 +1,9 @@
+﻿chrome.alarms.onAlarm.addListener((alarm) => {
+  //alert
+  chrome.windows.create({
+    width : 500,
+    height : 200,
+    type : 'popup',
+    url : 'alert.html'
+  });
+});
---
+++ myscript.js
@@ -0,0 +1,24 @@
+﻿const alarm_id = 'alarm_001';
+
+function setAlarm(event) {
+  let minutes = parseFloat(event.target.value);
+  chrome.browserAction.setBadgeText({text: 'ON'});
+  chrome.alarms.create({delayInMinutes: minutes});
+  chrome.storage.sync.set({minutes: minutes});
+  window.close();
+}
+function clearAlarm() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.alarms.clearAll();
+  window.close();
+}
+
+document.getElementById('id_Set').onclick = () => {
+  let dt = new Date();
+  let min = document.getElementById('id_Sec').value;
+  dt.setMinutes(dt.getMinutes() + parseInt(min));
+  chrome.alarms.create(alarm_id, {when: dt.getTime()});
+};
+document.getElementById('id_Clear').onclick = () => {
+  chrome.alarms.clear(alarm_id);
+};
